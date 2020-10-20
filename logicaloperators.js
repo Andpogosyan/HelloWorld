42d9fc3145aaf922e8300b8a9214b6f80b9875dd
Andronik Pogosyan <apogosyan.exceedteam@gmail.com>
2020-10-20 14:47:55 +0300

    added logicaloperators

---
+++ logicaloperators.js
@@ -0,0 +1,39 @@
+//задача 6
+
+//let age = +prompt('how ola are you?')
+
+//if (age >= 14 && age <= 90) {
+//    alert('good')
+//} else {
+//    alert('its bad')
+//}
+
+//задача 7
+
+
+
+//if (!(age >= 14 && age <= 90)) {
+//    alert('bad')
+//} else {
+//    alert('good')
+//}
+
+//задача 9
+
+//let userName = prompt('Кто там?')
+
+
+//if (userName == '' || userName == null) {
+//    alert('отменено')
+//} else if (userName != 'Админ') {
+//    alert('Я вас не знаю')
+//} else {
+//    let password = prompt('введите пароль')
+//    if (password == 'я главный') {
+//        alert('здравстуйте')
+//    } else if (userName == '' || userName == null) {
+//      alert('отменено')
+//    } else {
+//       alert('неверный пароль')
+//    }
+//}
