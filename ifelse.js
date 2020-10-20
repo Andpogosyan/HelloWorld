6e3e8511ac1312a3ba0afc322b14373f59396c1d
Andronik Pogosyan <apogosyan.exceedteam@gmail.com>
2020-10-20 13:31:00 +0300

    modified ifelse

--- ifelse.js
+++ ifelse.js
@@ -1,3 +1,5 @@
+//вторая задача
+
 let result = prompt("Какое официальное название 'JavaScript'");
 
 if (result == 'ECMAScript') {
@@ -5,3 +7,31 @@ if (result == 'ECMAScript') {
 } else {
     alert('Не знаете? ECMAScript!')
 }
+
+
+//третья задача
+
+let number = +prompt('введите число')
+
+if (number > 0) {
+    alert(1)
+} else if (number < 0) {
+    alert(-1)
+} else {
+    alert(0)
+}
+
+//четвертая задача
+
+let value;
+
+(a + b < 4) ? value = 'мало': value = 'много';
+
+//пятая задача
+
+let message;
+
+(login == 'Сотрудник') ? message = 'Привет':
+    (login == 'Директор') ? message = 'Здравствуйте' :
+    (login == '') ? message = 'Нет логина' :
+    message = '';
